421259c8c14e903096df270de7ae732d8ee84370
Talib Allauddin <taliballauddin@gmail.com>
2024-09-22 09:28:41 +0500

    testing ci/cd pipeline

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,6 +13,7 @@ export class TasksService {
   ) {}
 
   findAll(userId: number): Promise<Task[]> {
+    console.log(userId);
     return this.taskModel.find({ userId })
     .populate({
       path: 'labels',
