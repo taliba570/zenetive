653bde457e1a4293ec10ba4370307d4607191999
Talib Allauddin <taliballauddin@gmail.com>
2024-09-23 09:59:07 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Pomodoro Tracker API')
@@ -14,17 +15,6 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.enableCors({
-    origin: [
-      'https://master.d3muok6acru34g.amplifyapp.com/',
-      'https://master.d3muok6acru34g.amplifyapp.com',
-      /\.amplifyapp\.com$/,
-      '*',
-    ],  // Specify the exact URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,  // If you need to allow credentials (cookies, auth)
-  });
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
