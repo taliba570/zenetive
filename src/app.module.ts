a132369c3e180676f9b4643b73879cf85c61c693
Talib Allauddin <taliballauddin@gmail.com>
2024-09-26 21:37:57 +0500

    added cors middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { LabelsModule } from './labels/labels.module';
 import { PomodoroRecordModule } from './pomodoro-record/pomodoro-record.module';
 import { LoggerMiddleware } from './commons/middlewares/logger.middleware';
+import { CorsMiddleware } from './commons/middlewares/cors.middleware';
 
 @Module({
   imports: [
@@ -27,6 +28,9 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
+      .forRoutes('*');
+    consumer
+      .apply(CorsMiddleware)
       .forRoutes('*');
   }
 }
---
+++ src/commons/middlewares/cors.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.status(204).end();
+    }
+
+    next();
+  }
+}
