775f1568dd4b5102207636a22bd3b0ceef33f8a2
Talib Allauddin <taliballauddin@gmail.com>
2024-09-26 22:10:04 +0500

    updated cors logic

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.CORS.split(','),
+  const whiteList = [
+    'http://localhost:9000',
+    'https://master.d3muok6acru34g.amplifyapp.com',
+    '*'
+  ];
+  
+  const options = {
+    origin: process.env.NODE_ENV == 'prod' ? whiteList : true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  })
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    allowedHeaders: 'Content-Type,Accept,Authorization,email,x-request-id,request-type,X-Service-Identifier',
+    exposedHeaders: 'X-Service-Identifier'
+  };
+  const app = await NestFactory.create(AppModule, { cors: options });
 
   const config = new DocumentBuilder()
     .setTitle('Pomodoro Tracker API')
