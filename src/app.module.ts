cb6166ba597a4633ff67070880b03fc1db142552
Talib Allauddin <taliballauddin@gmail.com>
2024-09-28 09:37:16 +0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { AuthModule } from './auth/auth.module';
 import { LabelsModule } from './labels/labels.module';
 import { PomodoroRecordModule } from './pomodoro-record/pomodoro-record.module';
 import { LoggerMiddleware } from './commons/middlewares/logger.middleware';
-import { CorsMiddleware } from './commons/middlewares/cors.middleware';
 
 @Module({
   imports: [
@@ -28,9 +27,6 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes('*');
-    consumer
-      .apply(CorsMiddleware)
       .forRoutes('*');
   }
 }
