5f56ac611815abb985ebd8420acba425c24296fc
Talib Allauddin <taliballauddin@gmail.com>
2024-09-28 09:34:36 +0500

    added cors logic

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
+import { ImATeapotException, INestApplication } from '@nestjs/common';
 import * as bodyParser from 'body-parser'
 import * as compression from 'compression'
 
 async function bootstrap() {
   
-  const whiteList = [
-    'http://localhost:9000',
-    'http://localhost',
-    'localhost:9000',
-    'https://master.d3muok6acru34g.amplifyapp.com',
-    'master.d3muok6acru34g.amplifyapp.com',
-    'https://*.amplifyapp.com',
-    '*.amplifyapp.com',
-    '*'
-  ];
   
-  const options = {
-    origin: process.env.NODE_ENV == 'prod' ? whiteList : true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-    allowedHeaders: 'Content-Type,Accept,Authorization,email,x-request-id,request-type,X-Service-Identifier',
-    exposedHeaders: 'X-Service-Identifier'
-  };
-  const app = await NestFactory.create(AppModule, { cors: options });
-
-  app.enableCors(options);
+  const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'http://localhost:9000',
+    'https://master.d3muok6acru34g.amplifyapp.com'
+  ];
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    origin: function(origin, callback) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (
+        whitelist.includes(origin) ||
+        !!origin.match(/localhost$/)
+      ) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new ImATeapotException('Not allowed by CORS'), false);
+      }
+    }
+  })
   useBodyParser(app);
   app.use(compression());
   bootstrapSwagger(app);
